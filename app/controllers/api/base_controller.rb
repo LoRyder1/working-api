7ed56ccbc74110a4ab05532deb8b9ce06f33d879
Lovinder Pnag <lspnag@gmail.com>
2016-09-27 12:58:12 -0700

    public resource methods

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -53,7 +53,47 @@ module Api
       instance_variable_set("@#{resource_name}", resource)
     end
 
-    
+    # POST /api/ {plural_resouce_name}
+    def create
+      set_resource(resource_class.new(resource_params))
+
+      if get_resource.save
+        render :show, status: :created
+      else
+        render json: get_resource.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /api/{plural_resource_name}
+    def destroy
+      get_resource.destroy
+      head :no_content
+    end
+
+    # GET /api/ {plural_resource_name}
+    def index
+      plural_resource_name = "@#{resource_name.pluralize}"
+      resources = resource_class.where(query_params)
+                                .page(page_params[:page])
+                                .per(page_params[:page_size])
+
+      instance_variable_set(plural_resource_name, resources)
+      respond_with instance_variable_get(plural_resource_name)
+    end
+
+    # GET /api/ {plural_resource_name}/1
+    def show
+      respond_with get_resource
+    end
+
+    # PATCH/PUT /api/{plural_resource_name}/1
+    def update
+      if get_resource.update(resource_params)
+        render :show
+      else
+        render json: get_resource.errors, status: :unprocessable_entity
+      end
+    end
 
   end
 end
